a35150bfa58b8776842619aa5a2040766473d677
William Fecteau <william-fecteau@hotmail.com>
2024-01-25 19:55:28 -0500

    Added imported_bounds to default plot options

--- optimize_globaltraj.py
+++ optimize_globaltraj.py
@@ -21,12 +21,10 @@ debug: bool = True                                  # print console messages
 class PlotOptions:
     mincurv_curv_lin: bool = False                  # plot curv. linearization (original and solution based) (mincurv only)
     raceline: bool = True                           # plot optimized path
-    imported_bounds: bool = False                   # plot imported bounds (analyze difference to interpolated bounds)
-    raceline_curv: bool = False                     # plot curvature profile of optimized path
+    imported_bounds: bool = True                    # plot imported bounds (analyze difference to interpolated bounds)
     racetraj_vel: bool = False                      # plot velocity profile
     racetraj_vel_3d: bool = True                    # plot 3D velocity profile above raceline
     racetraj_vel_3d_stepsize: float = 0.5           # [m] vertical lines stepsize in 3D velocity profile plot
-    spline_normals: bool = False                    # plot spline normals to check for crossings
     mintime_plots: bool = False                     # plot states, controls, friction coeffs etc. (mintime only)
 
 @dataclass
@@ -55,8 +53,11 @@ class LapTimeOptions:
     top_speed_stepsize: float = 5.0                                         # step size to be applied
     file: str = "lap_time_matrix.csv"                                       # file name of the lap time matrix (stored in "outputs")
 
+OptimizationType = Literal['shortest_path', 'mincurv', 'mincurv_iqp', 'mintime']
+
+
 def launch_globaltraj_optimization(track_path: str, output_path: str, vehicle_param_file_path: str, 
-                                   opt_type: Literal['shortest_path', 'mincurv', 'mincurv_iqp', 'mintime'],
+                                   opt_type: OptimizationType,
                                    imp_opts: ImportOptions = ImportOptions(), plot_opts: PlotOptions = PlotOptions(),
                                    mintime_opts: MintimeOptions = MintimeOptions(), lap_time_mat_opts: LapTimeOptions = LapTimeOptions()):
     track_name = track_path.split('/')[-1]
